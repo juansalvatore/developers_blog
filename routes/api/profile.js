d4a4ba6c887cb160f755ce05d67cc49042bef007
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-18 12:27:19 -0300

    Creates profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
 
+// Load Profile Validation
+const validateProfileInput = require('../../validation/profile')
+
 // Load models
 const User = require('../../models/User')
 const Profile = require('../../models/Profile')
@@ -21,6 +24,7 @@ router.get(
   (req, res) => {
     const errors = {}
     Profile.findOne({ user: req.user.id })
+      .populate('user', 'name')
       .then(profile => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user'
@@ -33,13 +37,18 @@ router.get(
 )
 
 // @route   POST api/profile/test
-// @desc    Create user profile
+// @desc    Create or edit user profile
 // @access  Protected
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    const errors = {}
+    // Profile validation
+    const { errors, isValid } = validateProfileInput(req.body)
+
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
 
     // Get fields
     const profileFields = {}
@@ -58,11 +67,90 @@ router.post(
     }
     profileFields.social = {}
     if (req.body.youtube) profileFields.social.youtube = req.body.youtube
-
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram
     if (req.body.bio) profileFields.bio = req.body.bio
-    if (req.body.experience) profileFields.experience = req.body.experience
-    if (req.body.education) profileFields.education = req.body.education
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile))
+      } else {
+        // Create
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists'
+            res.status(400).json(errors)
+          }
+          // Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile))
+        })
+      }
+    })
   }
 )
+
+// @route   GET api/profile/all
+// @desc    Get array of profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {}
+  Profile.find({})
+    .populate('user', 'name')
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = 'There are no profiles'
+        res.status(404).json(errors)
+      } else {
+        return res.json(profiles)
+      }
+    })
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }))
+})
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {}
+  // params takes it from the url
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', 'name')
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err))
+})
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by User ID
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {}
+  // params takes it from the url
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', 'name')
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err =>
+      res.status(404).json({ profile: 'There is no profile for this user' })
+    )
+})
 
 module.exports = router
