93fe2849b8f9ace5aacf8d1b618a6c9086f625e5
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-19 14:54:24 -0300

    Create Post model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 router.get('/test', (req, res) => res.json({ test: 'Posts work' }))
 
+// @route   POST api/post
+// @desc    Create or edit user profile
+// @access  Protected
+
 module.exports = router
