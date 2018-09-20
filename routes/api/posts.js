2363918b35ac3a87812bff4418513eee9936b4ab
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-20 01:11:42 -0300

    Adds get and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -11,6 +11,50 @@ const validatePostInput = require('../../validation/post')
 
 router.get('/test', (req, res) => res.json({ test: 'Posts work' }))
 
+// @route   GET api/post
+// @desc    Get users profile
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find({})
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostfound: 'No posts found' }))
+})
+
+// @route   GET api/post/:id
+// @desc    Get user posts by id
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: 'No post found with that id' })
+    )
+})
+
+// @route   DELETE api/post/:id
+// @desc    Delete user posts by id
+// @access  Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id)
+          .then(post => {
+            if (post.user.toString() !== req.user.id) {
+              res.status(401).json({ notauthorized: 'User not authorized' })
+            }
+
+            post.remove().then(() => res.json({ success: true }))
+          })
+          .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+      })
+      .catch()
+  }
+)
+
 // @route   POST api/post
 // @desc    Create or edit user profile
 // @access  Protected
