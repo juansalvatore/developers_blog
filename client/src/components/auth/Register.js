9953ed874e9b2f1b6807afdc0e9be226dd381597
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-26 15:51:45 -0300

    Adds error checking and axios post to register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+// Styling
 import styled from 'styled-components'
 import { TextField, Button } from '@material-ui/core/'
+import FormHelperText from '@material-ui/core/FormHelperText'
+
+// HTTP requests (AXIOS)
+import axios from 'axios'
 
 export default class Register extends Component {
   state = {
@@ -24,11 +29,19 @@ export default class Register extends Component {
       password: this.state.password,
       password2: this.state.password2,
     }
-    console.log({ newUser })
+    // We added: "proxy": "http://localhost:5000" to package.json
+    // There is no need to add the rest of the route
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => {
+        this.setState({ errors: err.response.data })
+        console.log(this.state.errors)
+      })
   }
 
   render() {
-    const { name, email, password, password2 } = this.state
+    const { name, email, password, password2, errors } = this.state
     return (
       <RegisterWrapper>
         <Form onSubmit={this.onSubmit}>
@@ -39,28 +52,44 @@ export default class Register extends Component {
             label="Name"
             value={name}
             onChange={this.onChange}
+            error={errors.name ? true : false}
           />
+          <FormHelperText error={true}>{errors.name}</FormHelperText>
+
           <TextField
             name="email"
             label="Email Adress"
             value={email}
             onChange={this.onChange}
+            error={errors.email ? true : false}
           />
+          <FormHelperText error={true}>{errors.email}</FormHelperText>
+
           <TextField
             name="password"
             label="Password"
             type="password"
             value={password}
             onChange={this.onChange}
+            error={errors.password ? true : false}
           />
+          <FormHelperText error={true}>{errors.password}</FormHelperText>
           <TextField
             name="password2"
             label="Confirm Password"
             type="password"
             value={password2}
             onChange={this.onChange}
+            error={errors.password2 ? true : false}
           />
-          <Button color="primary" variant="raised" type="submit">
+          <FormHelperText error={true}>{errors.password2}</FormHelperText>
+
+          <Button
+            style={{ marginTop: 20 }}
+            color="primary"
+            variant="raised"
+            type="submit"
+          >
             Sign Up
           </Button>
         </Form>
@@ -86,6 +115,5 @@ const Form = styled.form`
     margin-bottom: 0px;
   }
   div {
-    margin-bottom: 10px;
   }
 `
