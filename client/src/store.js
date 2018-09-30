f9331eccef669af973a8f7f139606da2c150727a
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-30 19:50:12 -0300

    Correct redux store

--- client/src/store.js
+++ client/src/store.js
@@ -7,13 +7,13 @@ const initialState = {}
 const middleware = [thunk]
 
 // createStore takes: reducers, , middleware
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// )
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 export default store
