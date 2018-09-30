2066e524ed7b1d1c637ed8cca85f494b91dbb9ba
Juan Salvatore <juansalvatore@live.com.ar>
2018-09-30 19:42:53 -0300

    Remove redux middleware on production

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,16 @@ import rootReducer from './reducers'
 const initialState = {}
 const middleware = [thunk]
 
-// createStore takes: reducers, , middleware
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+if (process.env.NODE_ENV !== 'production') {
+  // createStore takes: reducers, , middleware
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   )
-)
-
+}
 export default store
