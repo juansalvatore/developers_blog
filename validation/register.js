74f53a253eb611f1c2a65632020a7887e9c8c49d
Juan Salvatore <juansalvatore@live.com.ar>
2018-10-03 12:14:30 -0300

    Change register password error

--- validation/register.js
+++ validation/register.js
@@ -29,7 +29,7 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be beaween 2 and 30 characters'
+    errors.password = 'Password must be beaween 6 and 30 characters'
   }
 
   if (Validator.isEmpty(data.password2)) {
