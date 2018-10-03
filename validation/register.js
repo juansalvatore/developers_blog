64dcd9da7c38f7ca21efe7cf04b1e7de2a5927de
Juan Salvatore <juansalvatore@live.com.ar>
2018-10-03 12:25:05 -0300

    Correct password typos in SignUp form

--- validation/register.js
+++ validation/register.js
@@ -29,7 +29,7 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be beaween 6 and 30 characters'
+    errors.password = 'Password must be between 6 and 30 characters'
   }
 
   if (Validator.isEmpty(data.password2)) {
